e3e925882f178c38d13b86a13b2902a9458eca1d
Solomon Hykes <solomon@dagger.io>
2023-10-30 11:30:35 -0700

    hello: a module to say hello
    Signed-off-by: Solomon Hykes <solomon@dagger.io>

---
+++ hello/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+// A Dagger module for saying hello to the world
+type Hello struct {
+	Greeting string `json:"greeting"`
+	Name     string `json:"name"`
+}
+
+// Change the greeting
+func (hello *Hello) WithGreeting(greeting string) *Hello {
+	hello.Greeting = greeting
+	return hello
+}
+
+// Change the name
+func (hello *Hello) WithName(name string) *Hello {
+	hello.Name = name
+	return hello
+}
+
+// Say hello to the world!
+func (hello *Hello) Message() string {
+	var (
+		greeting = hello.Greeting
+		name     = hello.Name
+	)
+	if greeting == "" {
+		greeting = "Hello"
+	}
+	if name == "" {
+		name = "World"
+	}
+	return fmt.Sprintf("%s, %s!", greeting, name)
+}
+
+// SHOUT HELLO TO THE WORLD!
+func (hello *Hello) Shout() string {
+	return strings.ToUpper(hello.Message() + "!!!!!!")
+}
