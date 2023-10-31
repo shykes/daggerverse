de0cbf179f6f1187be3015fc0c3b680a97a4fc3f
Solomon Hykes <solomon@dagger.io>
2023-10-30 22:19:58 -0700

    make: clean up and upgrade to 0.9
    Signed-off-by: Solomon Hykes <solomon@dagger.io>

--- make/main.go
+++ make/main.go
@@ -1,32 +1,18 @@
 package main
 
-import (
-	"context"
-)
-
 // A Dagger module to use make
 type Make struct{}
 
-func (m *Make) Make(ctx context.Context, dir *Directory, args []string) (*Directory, error) {
-	return make(dir, args), nil
-}
-
-func (dir *Directory) Make(ctx context.Context, args []string) (*Directory, error) {
-	return make(dir, args), nil
-}
-
-func make(dir *Directory, args []string) *Directory {
-	return image().
-		WithMountedDirectory("/src", dir).
-		WithWorkdir("/src").
-		WithExec(args).
-		Directory(".")
-}
-
-func image() *Container {
+// Execute the command 'make' in a directory, and return the modified directory
+func (m *Make) Make(dir *Directory, args []string, makefile Optional[string]) *Directory {
+	makefilePath := makefile.GetOr("Makefile")
 	return dag.
 		Container().
 		From("alpine:latest").
 		WithExec([]string{"apk", "add", "make"}).
-		WithEntrypoint([]string{"make"})
+		WithEntrypoint([]string{"make"}).
+		WithMountedDirectory("/src", dir).
+		WithWorkdir("/src").
+		WithExec(append([]string{"-f", makefilePath}, args...)).
+		Directory(".")
 }
