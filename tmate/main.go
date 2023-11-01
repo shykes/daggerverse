28497a39f1ac358b496d43334c305b074a51b5d2
Solomon Hykes <solomon@dagger.io>
2023-11-01 16:00:09 -0700

    tmate: cleanup
    Signed-off-by: Solomon Hykes <solomon@dagger.io>

--- tmate/main.go
+++ tmate/main.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"bufio"
+	"context"
+	"path"
+	"strings"
+)
+
 type Tmate struct{}
 
 func (t *Tmate) Source() *Directory {
@@ -9,7 +16,8 @@ func (t *Tmate) Source() *Directory {
 		Tree()
 }
 
-func (t *Tmate) BuildEnvUbuntu() *Container {
+// A build environment for tmate, with source code and all build dependencies installed
+func (t *Tmate) BuildEnv() *Container {
 	return dag.
 		Container().
 		From("ubuntu").
@@ -31,45 +39,60 @@ func (t *Tmate) BuildEnvUbuntu() *Container {
 		WithWorkdir("/src")
 }
 
-// Run tmate in a container
-func (t *Tmate) Tmate() *Container {
-	return t.BuildEnvUbuntu().
+// A build of tmate as a dynamically linked binary + required libraries
+func (t *Tmate) Dynamic(ctx context.Context) (*Directory, error) {
+	bundle := dag.Directory()
+	preBuild := t.BuildEnv()
+	postBuild := preBuild.
 		WithExec([]string{"autoupdate"}).
 		WithExec([]string{"./autogen.sh"}).
 		WithExec([]string{"./configure"}).
-		WithExec([]string{"make"}).
-		WithExec([]string{"make", "install"}).
-		WithDefaultArgs(ContainerWithDefaultArgsOpts{Args: []string{"tmate"}})
+		WithExec([]string{"make"})
+	bundle = bundle.WithFile("/bin/tmate", postBuild.File("tmate"))
+	libs, err := dynLibs(ctx, postBuild, "tmate")
+	if err != nil {
+		return nil, err
+	}
+	bundle = bundle.WithDirectory("/lib", libs)
+	return bundle, nil
+}
+
+// A static build of Tmate
+func (t *Tmate) Static() *File {
+	// FIXME: replace Dockerfile with pure Go
+	return t.Source().DockerBuild().File("tmate")
+}
+
+// Given a container and the path of an executable within it,
+//
+//	return a list of dynamic libraries required by the executable.
+func dynLibs(ctx context.Context, ctr *Container, binary string) (*Directory, error) {
+	// FIXME: inspect the binary contents in pure Go instead of shelling out to ldd
+	ldd, err := ctr.WithExec([]string{"ldd", binary}).Stdout(ctx)
+	if err != nil {
+		return nil, err
+	}
+	libs := dag.Directory()
+	// Parse the output of ldd
+	for scanner := bufio.NewScanner(strings.NewReader(ldd)); scanner.Scan(); {
+		line := scanner.Text()
+		fields := strings.Fields(line) // Split line by whitespace
+		if len(fields) != 4 {
+			continue
+		}
+		libPath := fields[2]
+		libName := path.Base(libPath)
+		lib := ctr.File(libPath)
+		libs = libs.WithFile(libName, lib)
+	}
+	return libs, nil
 }
 
 // Run tmate in a container
-func (t *Tmate) TmateWithEntrypoint() *Container {
-	return t.BuildEnvUbuntu().
-		WithExec([]string{"autoupdate"}).
-		WithExec([]string{"./autogen.sh"}).
-		WithExec([]string{"./configure"}).
-		WithExec([]string{"make"}).
-		WithExec([]string{"make", "install"}).
-		WithEntrypoint([]string{"tmate"})
+func (t *Tmate) Wrap(container *Container) *Container {
+	return container.WithFile("/bin/tmate", t.Static())
 }
 
-func (t *Tmate) BuildEnvWolfi() *Container {
-	return dag.
-		Wolfi().
-		Base().
-		WithPackages([]string{
-			"gcc",
-			"make",
-			"automake",
-			"perl",
-			"m4",
-			"autoconf",
-			"git",
-			"libtool",
-			"libevent-dev",
-			"ncurses-dev",
-		}).
-		Container().
-		WithMountedDirectory("/src", t.Source()).
-		WithWorkdir("/src")
+func (t *Tmate) WrapControl(container *Container) *Container {
+	return container
 }
