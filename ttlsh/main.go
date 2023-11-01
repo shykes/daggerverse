16e40ec244966e55e36a13cb6e1ff8023e1e1473
Erik Sipsma <erik@sipsma.dev>
2023-10-31 23:00:39 -0700

    make ttlsh easier to use by making args optional (#5)
    Signed-off-by: Erik Sipsma <erik@sipsma.dev>

--- ttlsh/main.go
+++ ttlsh/main.go
@@ -3,12 +3,24 @@ package main
 import (
 	"context"
 	"fmt"
+
+	"github.com/docker/docker/pkg/namesgenerator"
 )
 
 // A Dagger module to publish containers to ttl.sh, a throaway public registry
 type Ttlsh struct{}
 
 // Publish a container to ttl.sh
-func (m *Ttlsh) Publish(ctx context.Context, ctr *Container, repo, tag string) (string, error) {
-	return ctr.Publish(ctx, fmt.Sprintf("ttl.sh/%s:%s", repo, tag))
+func (m *Ttlsh) Publish(
+	ctx context.Context,
+	// the container to publish
+	ctr *Container,
+	// the repo to publish to, defaults to a random name
+	repo Optional[string],
+	// the tag to publish to, defaults to 10m
+	tag Optional[string],
+) (string, error) {
+	repoVal := repo.GetOr(namesgenerator.GetRandomName(0))
+	tagVal := tag.GetOr("10m")
+	return ctr.Publish(ctx, fmt.Sprintf("ttl.sh/%s:%s", repoVal, tagVal))
 }
