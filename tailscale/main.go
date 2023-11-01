dafe2b78ec1464b67487a87951a3ec2f6e9460bc
Solomon Hykes <solomon@dagger.io>
2023-10-31 17:23:15 -0700

    tailscale: bugfix and cleanup
    Signed-off-by: Solomon Hykes <solomon@dagger.io>

--- tailscale/main.go
+++ tailscale/main.go
@@ -12,13 +12,44 @@ const (
 	backendHostname = "backend"
 )
 
+func defaultBackend() *Service {
+	return dag.Container().From("index.docker.io/nginx").WithExposedPort(80).AsService()
+}
+
+// Inspect a backend service for debugging purposes
+func (m *Tailscale) Diagnostics(ctx context.Context, backend Optional[*Service]) (string, error) {
+	var out []string
+	backendService := backend.GetOr(defaultBackend())
+	ports, err := backendService.Ports(ctx)
+	if err != nil {
+		return "", err
+	}
+	out = append(out, fmt.Sprintf("%d exposed ports:", len(ports)))
+	for _, port := range ports {
+		number, err := port.Port(ctx)
+		if err != nil {
+			return "", err
+		}
+		out = append(out, fmt.Sprintf("- TCP/%d", number))
+	}
+	return strings.Join(out, "\n"), nil
+}
+
 // Expose a backend service on Tailscale at the given hostname, using the given Tailscale key.
 func (m *Tailscale) Gateway(ctx context.Context, hostname string, key *Secret, backend Optional[*Service]) (*Service, error) {
-	backendService := backend.GetOr(dag.Container().From("index.docker.io/nginx").AsService())
+	backendService := backend.GetOr(defaultBackend())
 	ports, err := backendService.Ports(ctx)
 	if err != nil {
 		return nil, err
 	}
+	gw := dag.
+		Container().
+		From("cgr.dev/chainguard/wolfi-base").
+		WithExec([]string{"apk", "add", "tailscale"}).
+		WithExec([]string{"apk", "add", "socat"}).
+		WithEnvVariable("TAILSCALE_HOSTNAME", hostname).
+		WithSecretVariable("TAILSCALE_AUTHKEY", key).
+		WithServiceBinding(backendHostname, backendService)
 	var proxyCmds []string
 	for _, port := range ports {
 		// FIXME: add UDP
@@ -35,25 +66,17 @@ func (m *Tailscale) Gateway(ctx context.Context, hostname string, key *Secret, b
 			proto,
 			number,
 			backendHostname))
+		// Expose the ports on the gateway container, as a convenience.
+		//  Technically this is not needed to expose them on tailscale,
+		//  but it is needed for `dagger up` to work with this function.
+		gw = gw.WithExposedPort(number)
+
 	}
 	proxyScript := strings.Join(proxyCmds, "\n")
 	script := proxyScript + "\n\n" + `
-	# This is a decoy port to allow using the gateway as a service (FIXME: this shouldn't be needed)
-	nc -l -p 9999 &
 	tailscaled --tun=userspace-networking --socks5-server=localhost:1055 &
 	tailscale login --hostname "$TAILSCALE_HOSTNAME" --authkey "$TAILSCALE_AUTHKEY"
 	tailscale up
 `
-	return dag.
-			Container().
-			From("cgr.dev/chainguard/wolfi-base").
-			WithExec([]string{"apk", "add", "tailscale"}).
-			WithExec([]string{"apk", "add", "socat"}).
-			WithEnvVariable("TAILSCALE_HOSTNAME", hostname).
-			WithSecretVariable("TAILSCALE_AUTHKEY", key).
-			WithServiceBinding(backendHostname, backendService).
-			WithExposedPort(9999).
-			WithExec([]string{"sh", "-c", script}).
-			AsService(),
-		nil
+	return gw.WithExec([]string{"sh", "-c", script}).AsService(), nil
 }
