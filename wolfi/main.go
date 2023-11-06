5c76c9e0da57b2f3e6e4a9462da8a8a6a7f36f7b
Solomon Hykes <solomon@dagger.io>
2023-11-05 20:49:13 -0600

    wolfi: add overlay support for Darren :)
    Signed-off-by: Solomon Hykes <solomon@dagger.io>

--- wolfi/main.go
+++ wolfi/main.go
@@ -6,30 +6,53 @@ const (
 	base = "cgr.dev/chainguard/wolfi-base"
 )
 
-// A Wolfi base configuration
+// Initialize a Wolfi base configuration
 func (w *Wolfi) Base() *Config {
 	return &Config{}
 }
 
-// A Wolfi container image
+// A Wolfi OS configuration
 type Config struct {
-	Packages []string `json:"packages"`
+	Packages []string     `json:"packages"`
+	Overlays []*Container `json:"overlays"`
 }
 
+// At a package to this configuration
 func (c *Config) WithPackage(name string) *Config {
 	return &Config{
 		Packages: append(c.Packages, name),
+		Overlays: c.Overlays,
 	}
 }
 
+// Add a list of packages to this configuration
 func (c *Config) WithPackages(packages []string) *Config {
 	return &Config{
 		Packages: append(c.Packages, packages...),
+		Overlays: c.Overlays,
 	}
 }
 
+// Add an overlay to the current configuration.
+// See https://twitter.com/ibuildthecloud/status/1721306361999597884
+func (c *Config) WithOverlay(image *Container) *Config {
+	return &Config{
+		Packages: c.Packages,
+		Overlays: append(c.Overlays, image),
+	}
+}
+
+// The container for this configuration
 func (c *Config) Container() *Container {
+	// 1. Apply Wolfi base
 	ctr := dag.Container().From(base)
+	// 2. Apply overlays
+	//  See Darren's request:
+	for _, overlay := range c.Overlays {
+		ctr = ctr.WithDirectory("/", overlay.Rootfs())
+	}
+	// 3. Install packages
+	// FIXME: download and merge directly instead of executing apk
 	for _, pkg := range c.Packages {
 		ctr = ctr.WithExec([]string{"apk", "add", pkg})
 	}
