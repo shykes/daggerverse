6c1b72b184f6b827cdc09bdeb6c180c40cbf2bd0
Solomon Hykes <solomon@dagger.io>
2024-02-01 15:07:49 -0800

    hello: optional figletContainer argument
    Signed-off-by: Solomon Hykes <solomon@dagger.io>

--- hello/main.go
+++ hello/main.go
@@ -6,24 +6,35 @@ import (
 	"strings"
 )
 
+var defaultFigletContainer = dag.
+	Container().
+	From("alpine:latest").
+	WithExec([]string{"apk", "add", "figlet"})
+
+// A Dagger module to say hello to the world
 type Hello struct{}
 
 // Say hello to the world!
-// If `giant` is set, write the message in giant multi-character letters.
-// If `shout` is set, make the message uppercase and add more exclamation points
-func (hello *Hello) Hello(ctx context.Context, greeting Optional[string], name Optional[string], giant Optional[bool], shout Optional[bool]) (string, error) {
+func (hello *Hello) Hello(ctx context.Context,
+	// An optional greeting (default is "hello")
+	greeting Optional[string],
+	// An optional name (default is "world")
+	name Optional[string],
+	// Encode the message in giant multi-character letters
+	giant Optional[bool],
+	// Make the message uppercase, and add more exclamation points
+	shout Optional[bool],
+	// Optional container for running the figlet tool
+	figletContainer Optional[*Container],
+) (string, error) {
 	message := fmt.Sprintf("%s, %s!", greeting.GetOr("Hello"), name.GetOr("world"))
 	if shout.GetOr(false) {
 		message = strings.ToUpper(message) + "!!!!!"
 	}
 	if giant.GetOr(false) {
+		ctr := figletContainer.GetOr(defaultFigletContainer).WithoutEntrypoint()
 		// Run 'figlet' in a container to produce giant letters
-		return dag.
-			Container().
-			From("alpine:latest").
-			WithExec([]string{"apk", "add", "figlet"}).
-			WithExec([]string{"figlet", message}).
-			Stdout(ctx)
+		return ctr.WithExec([]string{"figlet", message}).Stdout(ctx)
 	}
 	return message, nil
 }
