0d8450fb2554d17854566028538aa6d55cdc1640
Solomon Hykes <solomon@dagger.io>
2024-02-01 12:46:47 -0800

    hello: simplify, make quickstart-ready
    Signed-off-by: Solomon Hykes <solomon@dagger.io>

--- hello/main.go
+++ hello/main.go
@@ -6,58 +6,24 @@ import (
 	"strings"
 )
 
-func New(greeting Optional[string], name Optional[string]) *Hello {
-	return &Hello{
-		Greeting: greeting.GetOr(""),
-		Name:     name.GetOr(""),
-	}
-}
-
-// A Dagger module for saying hello to the world
-type Hello struct {
-	Greeting string
-	Name     string
-}
-
-// Change the greeting
-func (hello *Hello) WithGreeting(greeting string) *Hello {
-	hello.Greeting = greeting
-	return hello
-}
-
-// Change the name
-func (hello *Hello) WithName(name string) *Hello {
-	hello.Name = name
-	return hello
-}
+type Hello struct{}
 
 // Say hello to the world!
 // If `giant` is set, write the message in giant multi-character letters.
 // If `shout` is set, make the message uppercase and add more exclamation points
-func (hello *Hello) Message(ctx context.Context, giant Optional[bool], shout Optional[bool]) (string, error) {
-	var (
-		greeting = hello.Greeting
-		name     = hello.Name
-	)
-	if greeting == "" {
-		greeting = "Hello"
-	}
-	if name == "" {
-		name = "World"
-	}
-	message := fmt.Sprintf("%s, %s!", greeting, name)
+func (hello *Hello) Hello(ctx context.Context, greeting Optional[string], name Optional[string], giant Optional[bool], shout Optional[bool]) (string, error) {
+	message := fmt.Sprintf("%s, %s!", greeting.GetOr("Hello"), name.GetOr("world"))
 	if shout.GetOr(false) {
 		message = strings.ToUpper(message) + "!!!!!"
 	}
-	if !giant.GetOr(false) {
-		return message, nil
+	if giant.GetOr(false) {
+		// Run 'figlet' in a container to produce giant letters
+		return dag.
+			Container().
+			From("alpine:latest").
+			WithExec([]string{"apk", "add", "figlet"}).
+			WithExec([]string{"figlet", message}).
+			Stdout(ctx)
 	}
-
-	// Run 'figlet' in a container to produce giant letters
-	return dag.
-		Container().
-		From("alpine:latest").
-		WithExec([]string{"apk", "add", "figlet"}).
-		WithExec([]string{"figlet", message}).
-		Stdout(ctx)
+	return message, nil
 }
