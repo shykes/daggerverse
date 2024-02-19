7225d9159842f6ab0e1927543e11fee799afef58
Solomon Hykes <solomon@dagger.io>
2024-02-19 11:22:11 -0800

    slim: update to latest API, clean up implementation
    Signed-off-by: Solomon Hykes <solomon@dagger.io>

--- slim/main.go
+++ slim/main.go
@@ -2,11 +2,46 @@ package main
 
 import (
 	"context"
-	"fmt"
-	"strings"
 )
 
 type Slim struct{}
+
+// Slim down a container
+func (s *Slim) Slim(ctx context.Context, container *Container) (*Container, error) {
+	// Start an ephemeral dockerd
+	dockerd := dag.Docker().Engine()
+	docker := dag.Docker().Cli(DockerCliOpts{
+		Engine: dockerd,
+	})
+	// Load the input container into the dockerd
+	imgRef, err := docker.Import(container).Ref(ctx)
+	if err != nil {
+		return container, err
+	}
+	// Setup the slim container, attached to the dockerd
+	slim := dag.
+		Container().
+		// FIXME: choose image based on default architecture
+		From("index.docker.io/dslim/slim-arm").
+		WithServiceBinding("dockerd", dockerd).
+		WithEnvVariable("DOCKER_HOST", "tcp://dockerd:2375").
+		WithExec([]string{
+			"build",
+			"--tag", "slim-output:latest",
+			"--target", imgRef,
+			// "--show-clogs",
+		})
+	// Force execution of the slim command
+	slim, err = slim.Sync(ctx)
+	if err != nil {
+		return container, err
+	}
+	// Extract the resulting image back into a container
+	return docker.Image(DockerCliImageOpts{
+		Repository: "slim-output",
+		Tag:        "latest",
+	}).Export(), nil
+}
 
 func (s *Slim) Compare(ctx context.Context, container *Container) (*Container, error) {
 	slimmed, err := s.Slim(ctx, container)
@@ -19,81 +54,4 @@ func (s *Slim) Compare(ctx context.Context, container *Container) (*Container, e
 		WithMountedDirectory("before", slimmed.Rootfs()).
 		WithMountedDirectory("after", container.Rootfs())
 	return debug, nil
-}
-
-func (s *Slim) Slim(ctx context.Context, container *Container) (*Container, error) {
-	// Start an ephemeral dockerd
-	dockerd := dag.Dockerd().Service()
-	// Load the input container into the dockerd
-	if _, err := DockerLoad(ctx, container, dockerd); err != nil {
-		if err != nil {
-			return nil, err
-		}
-	}
-	// List images on the ephemeral dockerd
-	images, err := DockerImages(ctx, dockerd)
-	if err != nil {
-		return nil, err
-	}
-	if len(images) == 0 {
-		return nil, fmt.Errorf("Failed to load container into ephemeral docker engine")
-	}
-	firstImage := images[0]
-
-	// Setup the slim container, attached to the dockerd
-	slim := dag.
-		Container().
-		// FIXME: choose image based on default architecture
-		From("index.docker.io/dslim/slim-arm").
-		WithServiceBinding("dockerd", dockerd).
-		WithEnvVariable("DOCKER_HOST", "tcp://dockerd:2375").
-		WithExec([]string{
-			// "--debug",
-			"build",
-			"--tag", "slim-output:latest",
-			"--target", firstImage,
-			// "--show-clogs",
-		})
-
-	// Force execution of the slim command
-	slim, err = slim.Sync(ctx)
-	if err != nil {
-		return container, err
-	}
-
-	// Extract the resulting image back into a container
-	outputArchive := DockerClient(dockerd).WithExec([]string{
-		"image", "save",
-		"slim-output:latest",
-		// firstImage, // For now we output the un-slimeed image, while we debug
-		"-o", "output.tar"}).
-		File("output.tar")
-	return dag.Container().Import(outputArchive), nil
-}
-
-func DockerImages(ctx context.Context, dockerd *Service) ([]string, error) {
-	raw, err := DockerClient(dockerd).
-		WithExec([]string{"image", "list", "--no-trunc", "--format", "{{.ID}}"}).
-		Stdout(ctx)
-	if err != nil {
-		return nil, err
-	}
-	return strings.Split(raw, "\n"), nil
-}
-
-func DockerClient(dockerd *Service) *Container {
-	return dag.
-		Container().
-		From("index.docker.io/docker:cli").
-		WithServiceBinding("dockerd", dockerd).
-		WithEnvVariable("DOCKER_HOST", "tcp://dockerd:2375")
-}
-
-// Load a container into a docker engine
-func DockerLoad(ctx context.Context, c *Container, dockerd *Service) (string, error) {
-	client := DockerClient(dockerd).
-		WithMountedFile("/tmp/container.tar", c.AsTarball())
-	stdout, err := client.WithExec([]string{"load", "-i", "/tmp/container.tar"}).Stdout(ctx)
-	// FIXME: parse stdout
-	return stdout, err
 }
