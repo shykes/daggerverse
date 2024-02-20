64b2be35c3f354156d483c8de55f656057a8eac6
Solomon Hykes <solomon@dagger.io>
2024-02-19 18:52:31 -0600

    ollama: make model configurable
    Signed-off-by: Solomon Hykes <solomon@dagger.io>

--- ollama/src/main.py
+++ ollama/src/main.py
@@ -8,13 +8,14 @@ def llama2(prompt: str) -> str:
     return client().with_exec(["ollama", "pull", "llama2"]).with_exec(["ollama", "run", "llama2", prompt]).stdout()
 
 @function
-def client() -> dagger.Container:
-    return base().with_service_binding("server", server()).with_env_variable("OLLAMA_HOST", "server")
+def client(model: str) -> dagger.Container:
+    return base().with_service_binding("server", server(model)).with_env_variable("OLLAMA_HOST", "server")
 
 @function
 def base() -> dagger.Container:
     return dag.container().from_("index.docker.io/ollama/ollama").without_entrypoint()
 
 @function
-def server() -> dagger.Service:
-    return base().with_env_variable("OLLAMA_HOST", "0.0.0.0").with_exec(["ollama", "serve"]).with_exposed_port(11434).as_service()
+def server(model: str) -> dagger.Service:
+    return base().with_exec(["sh", "-c", "ollama serve & ollama pull " + model]).with_env_variable("OLLAMA_HOST", "0.0.0.0").with_exec(["ollama", "serve"]).with_exposed_port(11434).as_service()
+
