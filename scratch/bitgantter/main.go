2fe9160cbba94f007b176124fb081e963c0d5e36
Solomon Hykes <solomon@dagger.io>
2024-03-04 12:21:12 -0800

    scratch/bitgantter: proof of concept of wrapping docker-compose for compatibility
    Signed-off-by: Solomon Hykes <solomon@dagger.io>

--- scratch/bitgantter/main.go
+++ scratch/bitgantter/main.go
@@ -8,7 +8,39 @@ import (
 
 type Bitgantter struct{}
 
-// Dev environment with docker installed
-func (m *Bitgantter) Dev() *Container {
-	return dag.Docker().Cli().Container()
+func (m *Bitgantter) Build(
+	// Application source code
+	source *Directory,
+) *Container {
+	return source.DockerBuild()
 }
+
+// A containerized test environment
+func (m *Bitgantter) TestEnv(
+	ctx context.Context,
+	// The application container to test
+	app *Container,
+	// A docker-compose file configuring the test environment
+	// +optional
+	config *File,
+	) (*Container, error) {
+		if config == nil {
+			config = dag.CurrentModule().Source().File("./data/docker-compose.yml")
+		}
+		// Run an ephemeral Docker engine
+		// FIXME: optionally connect to an external engine
+		dockerd := dag.Docker().Engine()
+		// Initialize a docker client
+		docker := dag.Docker().Cli(DockerCliOpts{Engine: dockerd})
+		// Import the app image into the docker engine
+		appImage, err := docker.Import(app).LocalID(ctx)
+		if err != nil {
+		 	return nil, err
+		}
+		ctr := docker.Container().
+			WithEnvVariable("APP_IMAGE", appImage).
+			WithFile("/app/test/docker-compose.yml", config).
+			WithFile("/app/image.tar", app.AsTarball()).
+			WithWorkdir("/app")
+		return ctr, nil
+	}
