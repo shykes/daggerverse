0b45f792b1c237823de4a5c861b603170421405c
Solomon Hykes <solomon@dagger.io>
2024-03-04 17:23:47 -0800

    scratch/bitgantter: simplify API, docker-compose is part of the module logic
    Signed-off-by: Solomon Hykes <solomon@dagger.io>

--- scratch/bitgantter/main.go
+++ scratch/bitgantter/main.go
@@ -8,36 +8,19 @@ import (
 
 type Bitgantter struct{}
 
-func (m *Bitgantter) Build(
-	// Application source code
-	source *Directory,
-) *Container {
-	return source.DockerBuild()
-}
-
 // Execute end-to-end test in the given container
 func (m *Bitgantter) Test(ctx context.Context,
-	// The test container
+	// The app container to test
 	// +optional
-	test *Container,
+	app *Container,
+	// Custom test container
+	// +optional
+	testContainer *Container,
 	// The test command to execute
+	// +optional
 	command []string,
-	// A docker-compose file configuring the test environment
-	// +optional
-	config *File,
-	// Pass env variables to docker-compose with an env-file
-	// +optional
-	envFile *File,
-	// The image name of the test service that will run the test
-	// +default="local/test"
-	// +optional
-	name string,
-	// The image tag of the compose service that will run the test
-	// +default="latest"
-	// +optional
-	tag string,
 ) (string, error) {
-	env, err := m.TestEnv(ctx, test, config, envFile, name, tag)
+	env, err := m.TestEnv(ctx, app, testContainer)
 	if err != nil {
 		return "", err
 	}
@@ -45,6 +28,9 @@ func (m *Bitgantter) Test(ctx context.Context,
 	env, err = env.WithExec([]string{"docker-compose", "up", "-d", "--wait"}).Sync(ctx)
 	if err != nil {
 		return "", err
+	}
+	if command == nil {
+		command = []string{"curl", "http://app"}
 	}
 	testCmd := append(
 		[]string{"docker-compose", "exec", "test"},
@@ -53,68 +39,24 @@ func (m *Bitgantter) Test(ctx context.Context,
 	return env.WithExec(testCmd).Stdout(ctx)
 }
 
-// Open an interactive shell ready to execute end-to-end tests
-func (m *Bitgantter) Interactive(
-	ctx context.Context,
-	// The test container
-	// +optional
-	test *Container,
-	// A docker-compose file configuring the test environment
-	// +optional
-	config *File,
-	// Pass env variables to docker-compose with an env-file
-	// +optional
-	envFile *File,
-	// The image name of the test service that will run the test
-	// +default="local/test"
-	// +optional
-	name string,
-	// The image tag of the compose service that will run the test
-	// +default="latest"
-	// +optional
-	tag string,
-) (*Terminal, error) {
-	env, err := m.TestEnv(ctx, test, config, envFile, name, tag)
-	if err != nil {
-		return nil, err
-	}
-    // docker-compose up
-	env, err = env.WithExec([]string{"docker-compose", "up", "-d", "--wait"}).Sync(ctx)
-	if err != nil {
-		return nil, err
-	}
-	return env.Terminal(), nil
-	// return env.Terminal(ContainerTerminalOpts{Cmd: []string{"docker-compose", "exec", "test", "/bin/sh"}}), nil
-}
-
 // A containerized test environment
 func (m *Bitgantter) TestEnv(
 	ctx context.Context,
-	// The application container to test
+	// The app container to test
 	// +optional
-	test *Container,
-	// A docker-compose file configuring the test environment
+	app *Container,
+	// Custom test container
 	// +optional
-	config *File,
-	// Pass env variables to docker-compose with an env-file
-	// +optional
-	envFile *File,
-	// The image name of the test service that will run the test
-	// +default="local/test"
-	// +optional
-	name string,
-	// The image tag of the compose service that will run the test
-	// +default="latest"
-	// +optional
-	tag string,
+	testContainer *Container,
 	) (*Container, error) {
-		if config == nil {
-			config = dag.CurrentModule().Source().File("./data/docker-compose-simple.yml")
-		}
-		if test == nil {
-			test = dag.Wolfi().Container(WolfiContainerOpts{
+		composeFile := dag.CurrentModule().Source().File("./data/docker-compose-simple.yml")
+		if testContainer == nil {
+			testContainer = dag.Wolfi().Container(WolfiContainerOpts{
 				Packages: []string{"curl"},
 			})
+		}
+		if app == nil {
+			app = dag.Container().From("index.docker.io/nginx")
 		}
 		// Run an ephemeral Docker engine
 		// FIXME: optionally connect to an external engine
@@ -123,18 +65,24 @@ func (m *Bitgantter) TestEnv(
 		docker := dag.Docker().Cli(DockerCliOpts{Engine: dockerd})
 		// Import the test image into the docker engine
 		testImage, err := docker.
-			Import(test). // Import the app container into docker
-			Duplicate(name, tag). // Tag the app container
+			Import(testContainer). // Import the app container into docker
+			Duplicate("test", "dev"). // Tag the test container as 'test:dev'
+			Ref(ctx)
+		if err != nil {
+		 	return nil, err
+		}
+		// Import the app image
+		appImage, err := docker.
+			Import(app).
+			Duplicate("app", "dev"). // Tag the app container as 'app:dev'
 			Ref(ctx)
 		if err != nil {
 		 	return nil, err
 		}
 		ctr := docker.Container().
 			WithEnvVariable("TEST_IMAGE", testImage).
-			WithFile("/test/docker-compose.yml", config).
+			WithEnvVariable("APP_IMAGE", appImage).
+			WithFile("/test/docker-compose.yml", composeFile).
 			WithWorkdir("/test")
-		if envFile != nil {
-			ctr = ctr.WithFile("/test/.envrc", envFile)
-		}
 		return ctr, nil
 	}
